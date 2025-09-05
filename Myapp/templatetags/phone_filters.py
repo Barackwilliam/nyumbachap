199e50c25c8f10fe1220e773e67884b4a9040d90
barackwilliam <barackwilliam12@gmail.com>
2025-09-05 13:12:29 +0300

    test

---
+++ Myapp/templatetags/phone_filters.py
@@ -0,0 +1,20 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def to_whatsapp(phone):
+    """
+    Badilisha namba kuwa WhatsApp international format
+    """
+    if not phone:
+        return ""
+    
+    # Ondoa spaces na plus
+    phone = phone.replace(" ", "").replace("+", "")
+    
+    # Ikiwa inaanza na 0 → badilisha kuwa 255...
+    if phone.startswith("0"):
+        phone = "255" + phone[1:]
+    
+    return phone
